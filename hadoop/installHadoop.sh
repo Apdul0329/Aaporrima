92b879743621dc739d16ae17102002bbee0c277d
psy337337 <psy337337@naver.com>
2023-04-04 20:52:39 +0900

    Update installHadoop.sh

--- hadoop/installHadoop.sh
+++ hadoop/installHadoop.sh
@@ -3,9 +3,9 @@
 
 # install hadoop & make symbolic link
 cd /home/hadoop
-wget https://downloads.apache.org/hadoop/common/hadoop-3.3.4/hadoop-3.3.4.tar.gz
-tar xzf hadoop-3.3.4.tar.gz
-ln -s /home/hadoop/hadoop-3.3.4 ~/hadoop;
+wget https://downloads.apache.org/hadoop/common/hadoop-3.2.4/hadoop-3.2.4.tar.gz
+tar xzf hadoop-3.2.4.tar.gz
+ln -s /home/hadoop/hadoop-3.2.4 ~/hadoop;
 
 # Setting hadoop's environment variables
 cat ./Aaporrima/hadoop/bashrc.txt >> /home/hadoop/.bashrc
