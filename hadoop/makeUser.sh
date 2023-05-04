f7988aee351a8473c81952d03c652172a7f8bb23
Apdul0329 <98734140+Apdul0329@users.noreply.github.com>
2023-05-04 22:05:09 +0900

    STYLE: Change for test
    Change for test

--- hadoop/makeUser.sh
+++ hadoop/makeUser.sh
@@ -4,11 +4,11 @@
 a=$(cat /etc/passwd | grep hadoop)
 if [ "$a" != "" ]; then
 	echo "exists user hadoop, delete and remake"
-	sudo userdel -rf hadoop
+	echo "ubuntu" | sudo userdel -rf hadoop
 fi
 
 #make hadoop user
-sudo useradd -m hadoop
+echo "ubuntu" | sudo useradd -m hadoop
 echo 'hadoop:hadoop' | sudo chpasswd
-sudo chown hadoop:hadoop /home/hadoop
-sudo usermod -s /bin/bash hadoop
+echo "ubuntu" | sudo chown hadoop:hadoop /home/hadoop
+echo "ubuntu" | sudo usermod -s /bin/bash hadoop
